17eddc6581fda54eacbb63d58dc491552b4b999a
Jose david Redondo <redondoperdomojosedavid@gmail.com>
2022-09-22 10:00:22 -0500

    precarga

--- components/Model.js
+++ components/Model.js
@@ -5,11 +5,42 @@ import { useLoader } from '@react-three/fiber'
 import { TextureLoader } from 'three'
 
 const Model = ({ color }) => {
+  // =====================
+  var textureManager = new THREE.LoadingManager()
+  textureManager.onProgress = function (item, loaded, total) {
+    // this gets called after any item has been loaded
+    console.log(item, loaded, total)
+  }
+
+  textureManager.onLoad = function () {
+    // all textures are loaded
+    // ...
+  }
+
+  var textureLoader = new THREE.ImageLoader(textureManager)
+  var myTextureArray = [
+    '/textura/conversetelaverder.webp',
+    '/textura/conversetelaazul.webp',
+    '/textura/conversetelablanco.webp',
+    '/textura/conversetelanegra.webp',
+    '/textura/tample.webp',
+    '/textura/conversetelaamarilla.webp'
+  ]
+  var myTexture = new THREE.Texture()
+  myTextureArray.push(myTexture)
+
+  textureLoader.load(myTextureArray[0], function (image) {
+    myTexture.image = image
+  })
+  const newColor = myTextureArray.find(
+    (image) => image === `/textura/${color}.webp`
+  )
+
   // const texture = new THREE.TextureLoader().load('/floor/piso1.webp')
   // const material = new THREE.MeshBasicMaterial({ map: texture })
   const obj = useLoader(OBJLoader, '/figure3d/converse2.obj')
   // const colorTexture = useTexture('/textura/converse.jpg')
-  const templa = new THREE.TextureLoader().load(`/textura/${color}.webp`)
+  const templa = new THREE.TextureLoader().load(newColor)
   obj.children[0].material = new THREE.MeshPhongMaterial({
     map: templa
   })
